9970872d7af52ce9756b2921dd2edf51d54e4481
Theo Salzmann <dev@on3iro.de>
2017-02-13 18:34:21 +0100

    Added root resolving

--- src/components/Navigation/tests/Navigation.test.js
+++ src/components/Navigation/tests/Navigation.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Navigation from '../';
+import Navigation from '../index';
 import Entry from '../Entry';
 
 describe('Navigation', () => {
