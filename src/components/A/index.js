25d8cf138d154175148d32b154dfe907c0a22412
Theo Salzmann <dev@on3iro.de>
2017-02-15 19:33:46 +0100

    Check if that looks better on jsdoc

--- src/components/A/index.js
+++ src/components/A/index.js
@@ -1,10 +1,10 @@
+import styled from 'styled-components';
+
 /**
   * A link to a certain page, represented by an anchor tag.
   *
-  * @module A
+  * @name A
   */
-
-import styled from 'styled-components';
 
 const A = styled.a`
   display: inline;
