68f8227f34d31ba1d48d78b880e4a2197cc62e3d
Theo Salzmann <dev@on3iro.de>
2017-02-15 19:04:57 +0100

    Added unit tests

--- src/containers/App/tests/index.test.js
+++ src/containers/App/tests/index.test.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from '../index';
+import Content from '../Content';
+import Wrapper from '../Wrapper';
 import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 
 describe('App', () => {
-  it('Should render <div />', () => {
+  it('Should render Wrapper', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find('div').exists()).toEqual(true);
+    expect(wrapper.find(Wrapper).exists()).toEqual(true);
   });
 
   it('Should render a <Header />', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(Header).length).toEqual(1);
+  });
+
+  it('Should render a <Footer />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer).length).toEqual(1);
   });
 });
