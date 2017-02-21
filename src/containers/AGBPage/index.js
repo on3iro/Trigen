4eeb9258378edc47787e7593ce40520a91b6f288
Theo Salzmann <dev@on3iro.de>
2017-02-21 15:22:21 +0100

    Added generic AGB

--- src/containers/AGBPage/index.js
+++ src/containers/AGBPage/index.js
@@ -6,10 +6,234 @@
 
 import React from 'react';
 
+import Wrapper from './Wrapper';
+
 
 const AGBPage = () => {
   return (
-    <h1>AGB's here</h1>
+    <Wrapper>
+      Allgemeine Geschäftsbedingungen der Firma BeispielShop Online<br />
+
+<br />
+<p>§1 Geltung gegenüber Unternehmern und Begriffsdefinitionen</p>
+(1) Die nachfolgenden Allgemeinen Geschäftbedingungen gelten für alle Lieferungen zwischen uns und einem Verbraucher
+in ihrer zum Zeitpunkt der Bestellung gültigen Fassung.<br />
+<br />
+Verbraucher ist jede natürliche Person, die ein Rechtsgeschäft zu Zwecken abschließt, die überwiegend weder
+ihrer gewerblichen noch ihrer selbständigen beruflichen Tätigkeit zugerechnet werden können (§ 13 BGB).
+<br />
+
+    <br />
+<p>§2 Zustandekommen eines Vertrages, Speicherung des Vertragstextes</p>
+(1) Die folgenden Regelungen über den Vertragsabschluss gelten für Bestellungen über unseren Internetshop http://www.beispielshop-online.de .<br />
+<br />
+<b>(2) Im Falle des Vertragsschlusses kommt der Vertrag mit<br />
+
+<div>
+<br />
+BeispielShop Online<br />
+Klaus Muster<br />
+Beispielstraße 42<br />
+D-98765 Musterstadt<br />
+Registernummer 12131415<br />
+Registergericht Amtsgericht Musterstadt<br />
+<br />
+</div>
+zustande.<br /></b>
+<br />
+(3) Die Präsentation der Waren in unserem Internetshop stellen kein rechtlich bindendes Vertragsangebot unsererseits dar,
+sondern sind nur eine unverbindliche Aufforderungen an den Verbraucher, Waren zu bestellen. Mit der Bestellung der gewünschten Ware gibt der Verbraucher ein für
+ihn verbindliches Angebot auf Abschluss eines Kaufvertrages ab.<br />
+(4) Bei Eingang einer Bestellung in unserem Internetshop gelten folgende Regelungen:
+Der Verbraucher gibt ein bindendes Vertragsangebot ab, indem er die in unserem Internetshop vorgesehene Bestellprozedur erfolgreich durchläuft.<br />
+<br />
+Die Bestellung erfolgt in folgenden Schritten:<br />
+<br />
+<div className="ml20">
+
+    1) Registrierung im Portal<br />
+
+    2) Button "Slots freischalten"<br />
+
+    3) Weiterleitung zu Paypal<br />
+
+    4) Nach erfolgreicher Zahlung sind Slots verfügbar<br />
+
+<br />
+</div>
+Der Verbraucher kann vor dem verbindlichen Absenden der Bestellung durch Betätigen der in dem von ihm verwendeten Internet-Browser enthaltenen „Zurück“-Taste nach
+Kontrolle seiner Angaben wieder zu der Internetseite gelangen, auf der die Angaben des Kunden erfasst werden und Eingabefehler berichtigen bzw. durch Schließen des
+Internetbrowsers den Bestellvorgang abbrechen.
+Wir bestätigen den Eingang der Bestellung unmittelbar durch eine automatisch generierte E-Mail („Auftragsbestätigung“). Mit dieser nehmen wir Ihr Angebot an.
+<br />
+<br />
+(5) Speicherung des Vertragstextes bei Bestellungen über unseren Internetshop : Wir senden Ihnen die Bestelldaten und unsere AGB per
+E-Mail zu. Die AGB können Sie jederzeit auch unter http://www.beispielshop-online.de/agb.html  einsehen.
+
+Ihre Bestelldaten sind aus Sicherheitsgründen nicht mehr über das Internet zugänglich.
+<br />
+
+
+    <br />
+<p>§3 Preise, Versandkosten, Zahlung, Fälligkeit</p>
+(1) Die angegebenen Preise enthalten die gesetzliche Umsatzsteuer und sonstige Preisbestandteile. Hinzu kommen etwaige Versandkosten.<br />
+<br />
+(2) Der Verbraucher hat die Möglichkeit der Zahlung per
+            PayPal
+
+.<br />
+<br />
+
+
+<br />
+<p>§4 Lieferung</p>
+(1) Sofern wir dies in der Produktbeschreibung nicht deutlich anders angegeben haben, sind alle von uns angebotenen Artikel sofort versandfertig.
+Die Lieferung erfolgt hier spätesten innerhalb von 1 Werktagen.
+Dabei beginnt die Frist für die Lieferung im Falle der Zahlung per Vorkasse am Tag nach
+Zahlungsauftrag an die mit der Überweisung beauftragte Bank und bei allen anderen Zahlungsarten am Tag nach Vertragsschluss zu laufen.
+Fällt das Fristende auf einen Samstag, Sonntag oder gesetzlichen Feiertag am Lieferort, so endet die Frist am nächsten Werktag.
+<br /><br />
+
+(2) Die Gefahr des zufälligen Untergangs und der zufälligen Verschlechterung der
+verkauften Sache geht auch beim Versendungskauf erst mit der Übergabe der Sache an den Käufer auf diesen über.  <br />
+
+
+<br />
+<p>§5 Eigentumsvorbehalt</p>
+Wir behalten uns das Eigentum an der Ware bis zur vollständigen Bezahlung des Kaufpreises vor. <br />
+
+
+            <br />
+
+****************************************************************************************************
+<br />
+<p>§6 Widerrufsrecht des Kunden als Verbraucher
+</p>
+
+<b>
+    Widerrufsrecht für Verbraucher<br /></b>
+    <br />
+    Verbrauchern steht ein Widerrufsrecht nach folgender Maßgabe zu, wobei Verbraucher jede natürliche Person ist,
+    die ein Rechtsgeschäft zu Zwecken abschließt, die überwiegend weder ihrer gewerblichen noch ihrer selbständigen
+    beruflichen Tätigkeit zugerechnet werden können:
+
+    <br />
+    <br />
+
+    <p>Widerrufsbelehrung</p>
+    <br />
+   <b>Widerrufsrecht</b><br />
+    <br />
+    Sie haben das Recht, binnen vierzehn Tagen ohne Angabe von Gründen diesen Vertrag zu widerrufen.
+    <br />
+
+    Die Widerrufsfrist beträgt vierzehn Tage, ab dem Tag des Vertragsabschlusses.
+        <br />
+        <br />
+
+
+    Um Ihr Widerrufsrecht auszuüben, müssen Sie uns<br />
+    <div>
+        BeispielShop Online<br />
+        Klaus Muster<br />
+        Beispielstraße 42<br />
+        D-98765 Musterstadt<br />
+        E-Mail shop@beispielshop.com<br />
+                    Telefax 05123 45678910<br />
+            </div>
+    mittels einer eindeutigen Erklärung (z.B. ein mit der Post versandter Brief, Telefax oder E-Mail) über Ihren Entschluss,
+    diesen Vertrag zu widerrufen, informieren. Sie können dafür das beigefügte Muster-Widerrufsformular verwenden, das jedoch nicht vorgeschrieben ist.
+    <br />
+    <br />
+    Zur Wahrung der Widerrufsfrist reicht es aus, dass Sie die Mitteilung über die Ausübung des Widerrufsrechts vor Ablauf der Widerrufsfrist absenden.
+    <br />
+    <br />
+
+    <b>Widerrufsfolgen</b><br /><br />
+    Wenn Sie diesen Vertrag widerrufen, haben wir Ihnen alle Zahlungen, die wir von Ihnen erhalten haben,
+    einschließlich der Lieferkosten (mit Ausnahme der zusätzlichen Kosten, die sich daraus ergeben,
+    dass Sie eine andere Art der Lieferung als die von uns angebotene, günstigste Standardlieferung gewählt haben),
+    unverzüglich und spätestens binnen vierzehn Tagen ab dem Tag zurückzuzahlen, an dem die Mitteilung über Ihren
+    Widerruf dieses Vertrags bei uns eingegangen ist. Für diese Rückzahlung verwenden wir dasselbe Zahlungsmittel,
+    das Sie bei der ursprünglichen Transaktion eingesetzt haben, es sei denn, mit Ihnen wurde ausdrücklich etwas anderes vereinbart;
+    in keinem Fall werden Ihnen wegen dieser Rückzahlung Entgelte berechnet.<br />
+<br />
+    Haben Sie verlangt, dass die Dienstleistungen  während der Widerrufsfrist beginnen soll,
+    so haben Sie uns einen angemessenen Betrag zu zahlen, der dem Anteil der bis zu dem Zeitpunkt,
+    zu dem Sie uns von der Ausübung des Widerrufsrechts hinsichtlich dieses Vertrags unterrichten,
+    bereits erbrachten Dienstleistungen im Vergleich zum Gesamtumfang der im Vertrag vorgesehenen Dienstleistungen entspricht.
+    <br />
+    <br />
+
+
+    <b>Ende der Widerrufsbelehrung</b>
+<br />
+<br />
+****************************************************************************************************
+
+<br />
+
+
+<p>§7 Widerrufsformular</p>
+
+<p>Muster-Widerrufsformular</p>
+(Wenn Sie den Vertrag widerrufen wollen, dann füllen Sie bitte dieses Formular aus und senden Sie es zurück.)<br />
+<div>
+An :<br />
+BeispielShop Online<br />
+Klaus Muster<br />
+Beispielstraße 42<br />
+D-98765 Musterstadt<br />
+E-Mail shop@beispielshop.com<br />
+<br />
+
+Hiermit widerrufe(n) ich/wir (*) den von mir/uns (*) abgeschlossenen Vertrag über den Kauf der folgenden Waren (*)/die Erbringung der folgenden Dienstleistung (*)<br />
+<br />
+_____________________________________________________<br />
+<br />
+Bestellt am (*)/erhalten am (*)<br />
+<br />
+__________________<br />
+<br />
+Name des/der Verbraucher(s)<br />
+<br />
+_____________________________________________________<br />
+<br />
+Anschrift des/der Verbraucher(s)<br />
+<br />
+<br />
+_____________________________________________________<br />
+<br />
+Unterschrift des/der Verbraucher(s) (nur bei Mitteilung auf Papier)<br />
+<br />
+__________________<br />
+<br />
+Datum<br />
+<br />
+__________________<br />
+<br />
+</div>
+(*) Unzutreffendes streichen.<br />
+
+
+<br />
+<p>§8 Gewährleistung
+</p>
+
+Es gelten die gesetzlichen Gewährleistungsregelungen.
+<br />
+
+
+<br />
+<p>§9 Vertragssprache</p>
+ Als Vertragssprache steht ausschließlich Deutsch zur Verfügung.<br />
+
+<br />
+<br />
+<p>Stand der AGB Feb.2017</p>
+
+<p><i><a href="http://www.agb.de">Gratis AGB</a> erstellt von agb.de</i></p>
+    </Wrapper>
   );
 };
 
