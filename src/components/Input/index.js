3ecf103479226bf946ae6b08e008c6c57a7141fe
Theo Salzmann <dev@on3iro.de>
2017-03-22 09:13:43 +0100

    Added red border to errored input fields

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -12,7 +12,7 @@ const StyledInput = styled.input`
   display: block;
   margin: 0 0 10px 0;
   padding: 5px 10px;
-  border: 1px solid #e3e3e3;
+  border: 1px solid ${props => props.error ? 'red' : '#e3e3e3'};
   border-radius: 4px;
   color: #333;
   outline: 0;
