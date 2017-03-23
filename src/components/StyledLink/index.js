654e33699b219ac78fcd774039bb631843a7433f
Theo Salzmann <dev@on3iro.de>
2017-03-23 10:53:12 +0100

    Fixed unknown-prop-error

--- src/components/StyledLink/index.js
+++ src/components/StyledLink/index.js
@@ -1,12 +1,19 @@
 /**
  * A simple styled react-router Link element.
   */
-
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 
-const StyledLink = styled(Link)`
+export const LinkWrapper = props => {
+  // Pulls off secondary prop to avoid react-unknown-prop warning on underlying <a/>
+  const { secondary, ...rest } = props;
+
+  return <Link {...rest} />;
+};
+
+const StyledLink = styled(LinkWrapper)`
   display: inline-block;
   margin: 5px;
   width: 100px;
