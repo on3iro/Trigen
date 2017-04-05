dbd057bd1e40c291e95ffcc64a1f6864912a3e87
Theo Salzmann <dev@on3iro.de>
2017-04-05 16:35:44 +0200

    Started extracting password gen form into its own component

--- src/containers/Auth/LoginForm.js
+++ src/containers/Auth/LoginForm.js
@@ -11,7 +11,7 @@ import { Field, reduxForm } from 'redux-form';
 import H1 from 'components/H1';
 import Button from 'components/Button';
 
-import RenderField from './RenderField';
+import FormField from 'components/FormField';
 
 
 export const validate = values => {
@@ -36,10 +36,10 @@ export const LoginForm = props => {
     <form onSubmit={props.handleSubmit}>
       <H1>Login</H1>
       <Field name="email" type="text"
-        component={RenderField} label="Email"
+        component={FormField} label="Email"
       />
       <Field name="password" type="password"
-        component={RenderField} label="Passwort"
+        component={FormField} label="Passwort"
       />
       <Button submit >Anmelden</Button>
     </form>
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -4,141 +4,18 @@
   * @namespace HomePage
   */
 
-import React, { PropTypes, Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import shortid from 'shortid';
-import styled from 'styled-components';
-
-import { fetchAccounts } from 'containers/AccountsPage/ducks/actions';
-import { getAccounts, getAccountStatus } from 'containers/AccountsPage/ducks/selectors';
-
-import Input from 'components/Input';
+import React from 'react';
 
 import Wrapper from './Wrapper';
+import PassGenForm from './PassGenForm';
 
 
-export class Home extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      suggestions: [],
-      domain: '',
-      username: '',
-    };
-  }
-
-  componentDidMount() {
-    if(!this.props.accountsFetched) {
-      this.props.fetchAccounts(shortid.generate);
-    }
-  }
-
-  // Autosuggest input
-  getSuggestions = (value) => {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-
-    return (
-      inputLength === 0
-        ? []
-        : this.props.accounts.filter(
-          account => {
-            const val = account.domain.toLowerCase().includes(inputValue);
-            return val;
-          }
-        )
-    );
-  };
-
-  getSuggestionValue = suggestion => suggestion.domain;
-
-  onDomainChange = (event, { newValue }) => {
-    this.setState({
-      domain: newValue,
-    });
-  }
-
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.setState({
-      suggestions: this.getSuggestions(value)
-    });
-  }
-
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
-    });
-  }
-
-  onSuggestionSelected = (e, { suggestion }) => {
-    this.setState({
-      username: suggestion.username
-    });
-  }
-
-  renderSuggestion = suggestion => {
-    return (
-      <div>
-        {`${suggestion.domain} - Username: ${suggestion.username}`}
-      </div>
-    );
-  };
-
-  // Username input
-  onUsernameChange = e => {
-    this.setState({
-      username: e.target.value,
-    });
-  }
-
-  clearUsername = () => {
-    this.setState({ username: '' });
-  }
-
-  render() {
-    const inputProps = {
-      placeholder: 'Domain eingeben',
-      value: this.state.domain,
-      onChange: this.onDomainChange,
-    };
-
-    return (
-      <Wrapper>
-        <Input
-          autoSuggest
-          getSuggestionValue={this.getSuggestionValue}
-          highlightFirstSuggestion={true}
-          inputProps={inputProps}
-          onSuggestionSelected={this.onSuggestionSelected}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          renderSuggestion={this.renderSuggestion}
-          suggestions={this.state.suggestions}
-        />
-        <Input
-          onFocus={this.clearUsername}
-          placeholder="Username eingeben"
-          value={this.state.username}
-          onChange={this.onUsernameChange}
-        />
-      </Wrapper>
-    );
-  }
+const Home = () => {
+  return (
+    <Wrapper>
+      <PassGenForm />
+    </Wrapper>
+  );
 }
 
-const mapStateToProps = state => {
-  return {
-    accounts: getAccounts(state),
-    accountsFetched: getAccountStatus(state).fetched,
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-    fetchAccounts,
-  }, dispatch);
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
