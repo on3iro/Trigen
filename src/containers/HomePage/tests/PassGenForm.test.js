55eb08b7863d77be858df657210bbb9c6bb97a72
Theo Salzmann <dev@on3iro.de>
2017-04-06 20:06:14 +0200

    Added quite a few unit tests for PassGenForm component

--- src/containers/HomePage/tests/PassGenForm.test.js
+++ src/containers/HomePage/tests/PassGenForm.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import LoadingSpinner from 'components/LoadingSpinner';
+import Input from 'components/Input';
+import Button from 'components/Button';
+
 import { PassGenForm } from '../PassGenForm';
 
 
-const setUp = newProps => {
-  const props = {
+const baseProps = {
     accountListStatus: {
       isLoading: false,
       fetched: false,
@@ -20,8 +23,13 @@ const setUp = newProps => {
       }
     ],
     authToken: 'asdf',
-    fetchAccounts: jest.fn(),
     userID: 1,
+};
+
+const setUp = newProps => {
+  const props = {
+    ...baseProps,
+    fetchAccounts: jest.fn(),
     ...newProps,
   };
 
@@ -82,5 +90,116 @@ describe('<PassGenForm />', () => {
 
       expect(enzymeWrapper.instance().getSuggestions('uuu')).toEqual([]);
     });
+  });
+
+  describe('getSuggestionValue()', () => {
+    it('should return suggestion value', () => {
+      const { enzymeWrapper } = setUp();
+
+      expect(enzymeWrapper.instance().getSuggestionValue({ username: 'don', domain: 'brrzz@brozz.net' }))
+      .toBe('brrzz@brozz.net');
+    });
+  });
+
+  describe('onDomainChange()', () => {
+    it('should handle domain change', () => {
+      const { enzymeWrapper } = setUp();
+
+      enzymeWrapper.instance().onDomainChange({}, { newValue: 'test@test.net' });
+      expect(enzymeWrapper.state()).toMatchSnapshot();
+    });
+  });
+
+  describe('onSuggestionsFetchRequested()', () => {
+    it('should handle suggestion fetching', () => {
+      const { enzymeWrapper } = setUp();
+
+      enzymeWrapper.instance().onSuggestionsFetchRequested({ value: 'test' });
+      expect(enzymeWrapper.state()).toMatchSnapshot();
+    });
+  });
+
+  describe('onSuggestionsClearRequested()', () => {
+    it('should clear suggestions in component state', () => {
+      const { enzymeWrapper } = setUp();
+
+      enzymeWrapper.setState({suggestions: [{domain: 'hi'}]});
+
+      expect(enzymeWrapper.state().suggestions).toEqual([{ domain: 'hi' }]);
+
+      enzymeWrapper.instance().onSuggestionsClearRequested();
+
+      expect(enzymeWrapper.state().suggestions).toEqual([])
+
+    });
+  });
+
+  describe('onSuggestionSelected()', () => {
+    it('should populate username with suggestion value', () => {
+      const { enzymeWrapper } = setUp();
+
+      enzymeWrapper.instance().onSuggestionSelected({}, { suggestion: { domain: 'cthulhu@was.here', username: 'fthang'}});
+
+      expect(enzymeWrapper.state().username).toBe('fthang');
+    });
+  });
+
+  describe('renderSuggestion()', () => {
+    it('should return <div> and domain and username', () => {
+      const { enzymeWrapper } = setUp();
+
+      expect(enzymeWrapper.instance().renderSuggestion({ domain: 'howdy@bam.de', username: 'lucky luke'}))
+        .toEqual(<div>howdy@bam.de - Username: lucky luke</div>);
+    });
+  });
+
+  describe('onUsernameChange()', () => {
+    it('should update username inside component state', () => {
+      const { enzymeWrapper } = setUp();
+
+      enzymeWrapper.instance().onUsernameChange({ target: { value: 'the devil'} });
+
+      expect(enzymeWrapper.state().username).toBe('the devil');
+    });
+  });
+
+  describe('clearUsername()', () => {
+    it('should empty username inside component state', () => {
+      const { enzymeWrapper } = setUp();
+
+      enzymeWrapper.setState({ username: 'princess bubblegum' });
+
+      expect(enzymeWrapper.state().username).toBe('princess bubblegum');
+
+      enzymeWrapper.instance().clearUsername();
+
+      expect(enzymeWrapper.state().username).toBe('');
+    });
+  });
+
+  it('should render <LoadingSpinner /> on accountList loading', () => {
+    const { enzymeWrapper } = setUp({ accountListStatus: {
+      ...baseProps.accountListStatus, isLoading: true
+    }});
+
+    expect(enzymeWrapper.find(LoadingSpinner).exists()).toBe(true);
+  });
+
+  it('should render <form>', () => {
+    const { enzymeWrapper } = setUp();
+
+    expect(enzymeWrapper.find('form').exists()).toBe(true);
+  })
+
+  it('should render <Input/>', () => {
+    const { enzymeWrapper } = setUp();
+
+    expect(enzymeWrapper.find(Input).length).toBe(2)
+  });
+
+  it('should render <Button/>', () => {
+    const { enzymeWrapper } = setUp();
+
+    expect(enzymeWrapper.find(Button).exists()).toBe(true);
   });
 });
